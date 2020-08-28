4954bd73b98fb122d8ca586b967a6fb10a1f7f6c
Leandro Bergamo <leandro.bergamo@hotmail.com>
2020-08-27 21:04:27 -0300

    Trabalho subindo

---
+++ TP03/SegundoExercicio/Prova/DAL.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace Prova
+{
+    class DAL
+    {
+        private static String strConexao = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=BDFarinha.mdb";
+        private static OleDbConnection conn = new OleDbConnection(strConexao);
+        private static OleDbCommand strSQL;
+        private static OleDbDataReader result;
+
+        public static void conecta()
+        {
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception)
+            {
+                Erro.setMsg("Problemas ao se conectar ao Banco de Dados");
+            }
+
+        }
+
+        public static void desconecta()
+        {
+            conn.Close();
+        }
+
+        public static void consultaUmCliente(string cliente)
+        {
+            String aux = "SELECT * FROM TabClientes A,TabVendasCliente B WHERE A.CNPJ = '" + cliente + "' AND A.CNPJ = B.CNPJ";
+            strSQL = new OleDbCommand(aux, conn);
+            result = strSQL.ExecuteReader();
+        }
+
+        public static void getProximo()
+        {
+            Erro.setErro(false);
+            if (result.Read())
+            {
+                Cliente.setCNPJ(result.GetString(0));
+                Cliente.setNome(result.GetString(1));
+                //VendaCliente.setCodigo(result.GetString(2));
+                VendaCliente.setData(result.GetString(4));
+                VendaCliente.setToneladas(result.GetString(5));
+                VendaCliente.setvalor(result.GetString(6));
+            }
+            else
+                Erro.setErro(true);
+        }
+
+    }
+}
---
+++ TP03/SegundoExercicio/Prova/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prova
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BLL.conecta();
+            if (Erro.getErro())
+                MessageBox.Show(Erro.getMsg());
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            BLL.desconecta();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            if (textBox1.Text != "")
+            {
+                Cliente.setCNPJ(textBox1.Text);
+                BLL.validaCNPJ();
+                DAL.getProximo();
+                int total = 0;
+                int valor;
+                chart1.Series[0].Points.Clear();
+                while (!Erro.getErro())
+                {
+                    if (radioButton2.Checked)
+                    {
+                        valor = int.Parse(VendaCliente.getToneladas());
+                    }
+                    else
+                    {
+                        valor = int.Parse(VendaCliente.getToneladas());
+
+                    }
+                    total += valor;
+                    textBox2.Text = Cliente.getNome();
+                    chart1.Series[0].Points.AddXY(VendaCliente.getData(), valor);
+                    DAL.getProximo();
+                }
+                textBox3.Text = "" + total;
+            }
+            else
+            {
+                MessageBox.Show("Digite um valor!");
+            }
+        }
+    }
+}
